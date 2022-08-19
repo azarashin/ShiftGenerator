798f0eea336106820b3fcd631b60561ac02873ff
azarashin <azarashin1979@gmail.com>
2022-08-19 22:10:50 +0900

    従業員の希望を読み込む・保存する機能を追加。

--- react_native/shift_generator/common/data_accessor.tsx
+++ react_native/shift_generator/common/data_accessor.tsx
@@ -1,6 +1,7 @@
 import Storage from 'react-native-storage';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+
 const storage: Storage = new Storage({
     // 最大容量
     size: 1000,
@@ -22,7 +23,7 @@ export function DASave_Slot(slot: string, required: number) {
     });
 }
 
-export function DALoad_Slot(slot: string, callback: (retRequired: number) => void) { 
+export function DALoad_Slot(slot: string, callback: (ret_required: number) => void) { 
     var key = 'slot:' + slot;
     storage.load({
         key : key
@@ -34,4 +35,47 @@ export function DALoad_Slot(slot: string, callback: (retRequired: number) => voi
         console.log('load failed.');
         callback(0);
       });
+}
+
+// 現時点でのTypeScript では連想配列のkey に列挙子を指定できないため、
+// enum の実体を、enum名と同じ文字列にしている…(他に良い方法ないか？)。
+export const enum WishType {
+  wish = 'wish', 
+  refuse = 'refuse', 
+  neutral = 'neutral'
+}
+
+export declare type WishData = {
+  [index: string]: WishType
+}
+
+export function DALoad_StaffWish(staff: string, date: Date, callback: (ret_wish: WishData) => void) : void
+{
+  var key = 'wish:' + staff + ':' + date.getFullYear() + ':' + (date.getMonth() + 1);
+  storage.load({
+      key : key
+    }).then((data) => {
+      // 読み込み成功時処理
+      callback(data.wish);
+    }).catch(err => {
+      // 読み込み失敗時処理
+      console.log('load failed.');
+      callback({});
+    });
+    console.log(date); 
+    console.log(key); 
+}
+
+
+export function DASave_StaffWish(staff: string, date: Date, target : WishData) : void
+{
+  var key = 'wish:' + staff + ':' + date.getFullYear() + ':' + (date.getMonth() + 1);
+  storage.save({
+      key: key, 
+      data: {
+          wish: target
+      }
+  });
+  console.log(date); 
+  console.log(key); 
 }
