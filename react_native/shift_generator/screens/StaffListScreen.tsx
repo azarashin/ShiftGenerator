7962eab9dec9c0388c5f37997cec70b488650f6c
azarashin <azarashin1979@gmail.com>
2022-08-19 22:12:22 +0900

    体裁を整える。

--- react_native/shift_generator/screens/StaffListScreen.tsx
+++ react_native/shift_generator/screens/StaffListScreen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, FlatList , Button } from 'react-native';
+import { StyleSheet, Text, View, FlatList , Button } from 'react-native'
 import {useNavigation} from '@react-navigation/native'
 
 export default function StaffListScreen() {
