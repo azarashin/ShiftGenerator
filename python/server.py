40ad6b95c2277e6a30ea36c6461cae62220070e8
azarashin <azarashin1979@gmail.com>
2022-08-21 18:31:21 +0900

    サーバアプリを追加（まだリクエストを受け付けるだけ）

---
+++ python/server.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(
+    app,
+    supports_credentials=True
+)
+
+@app.route('/')
+def index():
+    return 'Hello World'
+
+@app.route('/shift_generate', methods=["POST"])
+def shift_generate():
+    req = request.json
+    staff_data = req.get("staff_data")
+    required = req.get("required")
+    conditions = req.get("conditions")
+    print(staff_data)
+    print(required)
+    print(conditions)
+    return {'result': 'ok'}
+
+if __name__ == "__main__":
+    app.run(debug=True)
