33e2d4c2111b91a71126618246bf5d11de0be0bf
azarashin <azarashin1979@gmail.com>
2022-08-21 23:49:28 +0900

    スロット情報を文字列化する機能を追加。

--- python/core/shift_slot.py
+++ python/core/shift_slot.py
@@ -4,3 +4,6 @@ class ShiftSlot(object):
   def __init__(self, id, require):
     self.id = id
     self.require = require
+    
+  def __str__(self):
+    return '{}: {}'.format(self.id, self.require)
