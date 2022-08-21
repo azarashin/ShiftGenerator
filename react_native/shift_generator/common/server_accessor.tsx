1da07f6dab29233bfb86542e5683162a3597b7c0
azarashin <azarashin1979@gmail.com>
2022-08-21 18:25:55 +0900

    不具合修正。

--- react_native/shift_generator/common/server_accessor.tsx
+++ react_native/shift_generator/common/server_accessor.tsx
@@ -1,5 +1,4 @@
 import { StaffWishInfo } from '../common/data_accessor';
-import { stringify } from 'qs';
 import axios from 'axios';
 
 const server_address : string = 'http://127.0.0.1:5000';
@@ -17,7 +16,7 @@ export function HTTPRequest_GenerateShift(
     var data = {staff_data: staff_data, required: required, conditions: conditions};
 
     axios
-      .post(server_address + '/shift_generate', stringify(data))
+      .post(server_address + '/shift_generate', data)
       .then((res) => {
         if(res.data.auth){
           alert('認証OK');
